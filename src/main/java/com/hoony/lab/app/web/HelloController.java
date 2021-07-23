047df2de5a65e2ab538818804c81715a5739fde7
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-07-23 14:29:10 +0900

    add helloController and Dto with Test

---
+++ src/main/java/com/hoony/lab/app/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.hoony.lab.app.web;
+
+import com.hoony.lab.app.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hell world";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
