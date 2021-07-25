d8948cce0f73db866195cff5e41578dbed3c138c
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-07-25 18:28:46 +0900

    전체 조회 화면 만들기

---
+++ src/main/java/com/hoony/lab/app/service/posts/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.hoony.lab.app.service.posts;
+
+import com.hoony.lab.app.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
