fa0b2bbd91a885b654c54f90dd5c23611f6b684d
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-07-25 19:43:31 +0900

    게시글 수정/삭제 화면 만들기
    View Controller
    Api Controller
    Service
    .mustache

--- src/main/java/com/hoony/lab/app/service/posts/PostsService.java
+++ src/main/java/com/hoony/lab/app/service/posts/PostsService.java
@@ -26,16 +26,14 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(()
-                        -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()
-                        -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
 
@@ -44,5 +42,13 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
