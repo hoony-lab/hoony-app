a7fb0b36d96ea0c8add143906b37b78ad01f0f96
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-07-27 17:37:18 +0900

    feat: 구글 로그인 연동하기

--- src/main/java/com/hoony/lab/app/web/IndexController.java
+++ src/main/java/com/hoony/lab/app/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hoony.lab.app.web;
 
+import com.hoony.lab.app.config.auth.dto.SessionUser;
 import com.hoony.lab.app.service.posts.PostsService;
 import com.hoony.lab.app.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            System.out.println("############# " + user.getName());
+            System.out.println("############# " + user.getEmail());
+            System.out.println("############# " + user.getPicture());
+            model.addAttribute("userName", user.getEmail());
+        }
         return "index";
     }
 
